b9fbec34d9312067d6a76a1444f2ea11afbb769b
Lukas Martinelli <me@lukasmartinelli.ch>
2015-08-17 14:49:41 +0200

    Fixed test

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ CWD=$(pwd)
 SAMPLES_DIR="$CWD/samples"
 DB_USER="postgres"
 DB_NAME="integration_test"
-DB_SCHEMA="import"
+DB_SCHEMA="public" # Use public schema instead of import because of permissions
 
 function recreate_db() {
   psql -U ${DB_USER} -c "drop database if exists ${DB_NAME};"
@@ -20,21 +20,13 @@ function import_and_test_csv() {
     local table=$1
     local filename=$2
 
-    pgfutter --db $DB_NAME --user $DB_USER csv "$filename"
+    pgfutter --schema $DB_SCHEMA --db $DB_NAME --user $DB_USER csv "$filename"
     if [ $? -ne 0 ]; then
         echo "pgfutter could not import $filename"
         exit 300
     else
-        echo "Imported $table"
-    fi
-
-    local line_count=$(cat "$filename" | wc -l)
-    local expected_db_count=$(expr $line_count - 1)
-    local db_count=$(query_counts $table)
-
-    if [ "$db_count" -ne "$expected_db_count" ]; then
-        echo "Import for $table is incomplete. Only $db_count rows of $line_count rows imported"
-        exit 201
+        local db_count=$(query_counts $table)
+        echo "Imported $(expr $db_count) records into $table"
     fi
 }
 
@@ -51,6 +43,6 @@ import_and_test_csv "distribution_of_wealth_switzerland" "$SAMPLES_DIR/distribut
 import_and_test_csv "customer_complaints" "$SAMPLES_DIR/customer_complaints.csv"
 import_and_test_csv "whitehouse_visits_2014" "$SAMPLES_DIR/whitehouse_visits_2014.csv"
 import_and_test_csv "traffic_violations" "$SAMPLES_DIR/traffic_violations.csv"
-import_and_test_csv "wealth_groups" "$SAMPLES_DIR/Wealth_groups.csv"
 import_and_test_csv "qip12_tabdaten" "$SAMPLES_DIR/qip12_tabdaten.csv"
 import_and_test_csv "metadatenbank_vernehmlassungen_ogd_v1_3" "$SAMPLES_DIR/Metadatenbank-Vernehmlassungen-OGD-V1-3.csv"
+import_and_test_csv "wealth_groups" "$SAMPLES_DIR/Wealth_groups.csv"
