a08d83a564ba8f762c9e8b651d4ca5d5c51cba3e
Benjamin Flesch <bflesch@bf-itservice.de>
2018-04-11 17:37:47 +0200

    try fix zerobyte pg import bug

--- csv.go
+++ csv.go
@@ -76,7 +76,9 @@ func copyCSVRows(i *Import, reader *csv.Reader, ignoreErrors bool, delimiter str
 		//Loop ensures we don't insert too many values and that
 		//values are properly converted into empty interfaces
 		for i, col := range record {
-			cols[i] = col
+			cols[i] = strings.Replace(col, "\x00", "", -1)
+			// bytes.Trim(b, "\x00")
+			// cols[i] = col
 		}
 
 		err = i.AddRow(cols...)
